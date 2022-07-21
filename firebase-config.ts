6a2f5048d081187b2af5551d9bf8f74db03ced9c
Parth099 <parthnpatel715@gmail.com>
2022-07-20 20:24:15 -0400

    added firebase to app

---
+++ firebase-config.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCTAWf19XUtFxaylypBNQQiKJMle-IS9fo",
+    authDomain: "react-firebase-blogging-app.firebaseapp.com",
+    projectId: "react-firebase-blogging-app",
+    storageBucket: "react-firebase-blogging-app.appspot.com",
+    messagingSenderId: "1046778962501",
+    appId: "1:1046778962501:web:ff60eeaad6872c5605b92c",
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
