03b626089f29c3b89f579862ee281553b4b3613e
Parth099 <parthnpatel715@gmail.com>
2022-07-21 20:11:42 -0400

    added nav bar, added website colors, ready for auth contexts

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
+import { Outlet } from "react-router-dom";
 import "./App.css";
-
+import Navbar from "./components/Navbar";
+/*
+App will be the entry point of the app (haha)
+This will be a place for <Outlet> and the Auth/Storage Providers
+*/
 function App() {
-    return <div className="font-bold header-font">hi</div>;
+    return (
+        <div>
+            <Navbar />
+            <Outlet />
+        </div>
+    );
 }
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
     content: ["./src/**/*.{js,jsx,ts,tsx}"],
     theme: {
-        extend: {},
+        extend: { colors: { sp1: "#df4c73", sp2: "#ffffff", sp3: "#286fb4", sp4: "#b0dde4", sp5: "#addae1" } },
     },
     plugins: [],
 };
