acd6357090af058f54abd6aadfc77f9ee55cdfa1
Parth099 <parthnpatel715@gmail.com>
2022-07-26 22:44:44 -0400

    new StorageProvider provides profile information and listens for realtime profile updates

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Outlet } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import { AuthProvider } from "./contexts/authContext";
+import { StorageProvider } from "./contexts/storageContext";
 /*
 App will be the entry point of the app (haha)
 This will be a place for <Outlet> and the Auth/Storage Providers
@@ -9,8 +10,10 @@ This will be a place for <Outlet> and the Auth/Storage Providers
 function App() {
     return (
         <AuthProvider>
-            <Navbar />
-            <Outlet />
+            <StorageProvider>
+                <Navbar />
+                <Outlet />
+            </StorageProvider>
         </AuthProvider>
     );
 }
--- src/contexts/storageContext.tsx
+++ src/contexts/storageContext.tsx
@@ -1,5 +1,7 @@
-import { CollectionReference, DocumentReference } from "firebase/firestore";
-import { createContext, useContext } from "react";
+import { collection, CollectionReference, doc, DocumentReference, getDoc, onSnapshot, setDoc } from "firebase/firestore";
+import { createContext, useContext, useEffect, useState } from "react";
+import { database } from "../../firebase-config";
+import { useAuth } from "./authContext";
 import { Nullable, ReactChildren } from "./models";
 
 /*
@@ -8,9 +10,16 @@ This context tracks the blog collection ref
 
 */
 
+interface ProfileData {
+    email: string;
+    username: string;
+    profilePictire: string;
+    dateCreated: Date;
+}
+
 interface StorageContextObject {
     blogRef: CollectionReference;
-    profileRef: Nullable<DocumentReference>;
+    profileData: Nullable<ProfileData>;
 }
 const StorageContext = createContext<Nullable<StorageContextObject>>(null);
 
@@ -19,5 +28,67 @@ export const useStorage = () => {
 };
 
 export function StorageProvider({ children }: ReactChildren) {
-    return <StorageContext.Provider value={null}>{children}</StorageContext.Provider>;
+    //need this to locate profile
+    const authContext = useAuth();
+
+    const [profileData, setProfileData] = useState<Nullable<ProfileData>>(null);
+
+    //initial value of db
+    const value: StorageContextObject = {
+        blogRef: collection(database, "main:blogs"),
+        profileData: profileData,
+    };
+
+    const DocumentExists = async (ref: DocumentReference) => {
+        const docSnap = await getDoc(ref);
+        //if exists we return data
+        return docSnap.exists() ? docSnap.data() : false;
+    };
+
+    useEffect(() => {
+        //if there is a user logged in
+        if (authContext?.currentUser && authContext.currentUser.email) {
+            //var for email as it is a KEY identifer
+            const email = authContext.currentUser.email;
+
+            const profileRef = doc(database, `main:profiles/${email}`);
+            DocumentExists(profileRef).then((isExists) => {
+                //if doc exists we dont need to do anything
+                if (isExists) return;
+
+                //create it with the current data
+                setDoc(profileRef, {
+                    email,
+                    datecreated: new Date(),
+                    username: "",
+                    profilePicture: "",
+                });
+            });
+        }
+    }, [authContext]);
+
+    //per a change in the profile state in firebase OUR state will be updated
+    useEffect(() => {
+        //if no current user exists dont run anything
+        if (!authContext?.currentUser) return;
+
+        const email = authContext.currentUser.email;
+        //r-val is a listener remover
+        return onSnapshot(doc(database, `main:profiles/${email}`), (doc) => {
+            const data = doc.data();
+            if (data) {
+                //fool typescript
+                const profileData = {
+                    email: data.email as string,
+                    username: data.username as string,
+                    profilePictire: data.profilePicture as string,
+                    dateCreated: data.dataCreated as Date,
+                };
+
+                setProfileData(profileData);
+            }
+        });
+    }, [authContext]);
+
+    return <StorageContext.Provider value={value}>{children}</StorageContext.Provider>;
 }
